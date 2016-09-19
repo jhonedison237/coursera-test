b62659a91913e48e4ea4acddcdefcb0040963546
Jhon Edison Londoño Hernandez <Jhon Edison Londoo Hernandez>
2016-09-19 00:38:26 -0500

    the solution to the assignment 1

--- assignment1/scripts/app.js
+++ assignment1/scripts/app.js
@@ -1,10 +1,34 @@
 (function () {
 'use strict';
 
-angular.module('myFirstApp', [])
+angular.module('LunchCheck', [])
 
-.controller('MyFirstController', function () {
+.controller('LunchCheckController', LunchCheckController);
 
-});
+LunchCheckController.Sinject = ['$scope'];
+function LunchCheckController($scope) {
+	$scope.dishes = "";
+	$scope.message = "";
+	$scope.color = "balck"
+	
+	$scope.checkDishes = function() {
+		
+		var arrayDishes = $scope.dishes.split(",");
+		var count = arrayDishes.length;
+		$scope.color = "";
+		
+		if ($scope.dishes == "") {
+			$scope.message = "Please enter data first";
+			$scope.color = "red";
+		} else if (count <=3){
+			$scope.message = "Enjoy!"
+			$scope.color = "green"
+		} else {
+			$scope.message = "Too much!"
+			$scope.color = "green"
+		}
+	};
+}
+
 
 })();
